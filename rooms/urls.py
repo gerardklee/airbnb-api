e08986d26c41ad0171fae3bcdfbabf224d55577d
Gerard Lee <gerardklee@hotmail.com>
2022-01-05 12:44:37 -0800

    changed from functional based view to class based view

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "rooms"
 
 urlpatterns = [
-	path("list", views.list_rooms)
+	path("list", views.RoomsList.as_view())
 ]
 
--- rooms/views.py
+++ rooms/views.py
@@ -1,10 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Room
 from .serializers import RoomSerializer
 
+class RoomsList(APIView):
+    def get(self, request, format=None):
+        rooms = Room.objects.all()
+        serialized_rooms = RoomSerializer(rooms, many=True)
+        return Response(serialized_rooms.data)
+
 @api_view(["GET"])
 def list_rooms(request):
-    rooms = Room.objects.all()
-    serialized_rooms = RoomSerializer(rooms, many=True)
-    return Response(data=serialized_rooms.data)
+    return Response()
