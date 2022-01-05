352e07c15b9e6cb261b8f653cf6c02ba45879be5
Gerard Lee <gerardklee@hotmail.com>
2022-01-05 12:43:59 -0800

    incorporated model serializers

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
+from users.serializers import UserSerializer
+from .models import Room
 
-class RoomSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=140)
-    price = serializers.IntegerField()
-    bedrooms = serializers.IntegerField()
-    instant_book = serializers.BooleanField()
+class RoomSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    class Meta:
+        model = Room
+        fields = [
+            "name",
+            "price",
+            "instant_book",
+            "user"
+        ]
