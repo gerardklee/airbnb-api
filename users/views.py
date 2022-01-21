5ab0bcc3de7ebb137ebd9a334ccdc39d91cd657a
Gerard Lee <gerardklee@hotmail.com>
2022-01-20 19:22:44 -0800

    PUT method added

--- users/views.py
+++ users/views.py
@@ -2,20 +2,38 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from .serializers import ReadUserSerializer
+from .serializers import ReadUserSerializer, WriteUserSerializer
+from .models import User
 
 class MeView(APIView):
     def get(self, request):
         my_serializer = ReadUserSerializer(request.user).data
         if request.user.is_authenticated:
             return Response(data=my_serializer, status=status.HTTP_200_OK)
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
     
     def put(self, request):
-        pass
+        print("me_view")
+        return Response(status=status.HTTP_200_OK)
 
-@api_view(["GET"])
+@api_view(["GET", "PUT"])
 def user_view(request, pk):
-    if request.method == "GET":
-        return Response({"message":"GET request received"})
-    pass
+    print("user_view")
+    try:
+        user = User.objects.get(pk=pk)
+        if request.method == "GET":
+            user_serializer = ReadUserSerializer(user)
+            return Response(data=user_serializer.data, status=status.HTTP_200_OK)
+        elif request.method == "PUT":
+            print("inside of PUT")
+            print("request_data: ", request.data)
+            user_serializer = WriteUserSerializer(user, data=request.data, partial=True)
+            if user_serializer.is_valid():
+                print("inside of is_valid")
+                user_serializer.save()
+                return Response(data=user_serializer.data, status=status.HTTP_200_OK)
+            return Response(user_serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
