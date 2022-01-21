30619c869fbba9ca19d5c3b92ce453ade6f85b01
Gerard Lee <gerardklee@hotmail.com>
2022-01-20 19:22:27 -0800

    write user serializer added

--- users/serializers.py
+++ users/serializers.py
@@ -31,3 +31,23 @@ class ReadUserSerializer(serializers.ModelSerializer):
             "date_joined",
             "favs"
         )
+
+class WriteUserSerializer(serializers.Serializer):
+    username   = serializers.CharField(max_length=140)
+    first_name = serializers.CharField(max_length=140)
+    last_name  = serializers.CharField(max_length=140)
+    email      = serializers.EmailField(max_length=254)
+
+    def valiate(self, data):
+        print("data in validated: ", data)
+        return data
+
+    def update(self, instance, validated_data):
+        print("instance in update: ", instance)
+        print("data in validated_data: ", validated_data)
+        instance.username   = validated_data.get("username", instance.username)
+        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.last_name  = validated_data.get("last_name", instance.last_name)
+        instance.email      = validated_data.get("email", instance.email)
+        instance.save()
+        return instance
