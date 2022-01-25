b990011bca62a3027c4e7605a6b752dc3918c725
Gerard Lee <gerardklee@hotmail.com>
2022-01-24 19:31:24 -0800

    united read and write serializers

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -2,6 +2,36 @@ from rest_framework import serializers
 from users.serializers import UserSerializer
 from .models import Room
 
+# Below class unifies the read room serializer and the write room serializer
+class RoomSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    class Meta:
+        model = Room
+        exclude = (
+            "modified",
+        )
+        read_only_fields = (
+            "user",
+            "id",
+            "created",
+            "updated"
+        )
+    def validate(self, data):
+        # validate for update/if there is instance on the serializer
+        print("validate starts")
+        if self.instance:
+            check_in = data.get("check_in", self.instance.check_in)
+            check_out = data.get("check_out", self.instance.check_out)
+
+        # validate for create
+        else:
+            check_in = data.get(data.check_in)
+            check_in = data.get(data.check_out)
+
+        if check_in == check_out:
+            raise serializers.ValidationError("Not enough time between changes")
+        return data
+
 class ReadRoomSerializer(serializers.ModelSerializer):
     user = UserSerializer()
     class Meta:
