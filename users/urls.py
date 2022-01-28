b5dd911c6723e8e71ab8d3ea9d8978f9ea863331
Gerard Lee <gerardklee@hotmail.com>
2022-01-27 18:45:08 -0800

    login url added

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,7 @@ app_name = "users"
 
 urlpatterns = [
     path("", views.UsersView.as_view()),
+    path("login/", views.login),
     path("me/", views.MeView.as_view()),
     path("<int:pk>/", views.user_view),
     path("<int:pk>/fav", views.toggle_fav),
